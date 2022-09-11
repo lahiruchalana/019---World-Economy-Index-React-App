3955407335530a94b2ebdcf6416f102a8e79e1b1
lahiruchalana <lahiruchalanairesh@gmail.com>
2022-09-11 16:22:40 +0530

    completed currency and stock market mapping

--- src/Views/Home/Home.js
+++ src/Views/Home/Home.js
@@ -12,8 +12,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Home() {
 
+    var button = {
+        borderRadius:'0px'
+    };
+
     var button_styles = {
-        marginTop:'10px'
+        marginTop:'10px',
+        borderRadius:'0px'
+    };
+
+    var percentage_green_style = {
+        color:'green'
+    };
+
+    var percentage_red_style = {
+        color:'red'
     };
 
 
@@ -23,7 +36,7 @@ function Home() {
                 <Col>
                     <div id='column_center'>
                         <br></br>
-                        <Button id='home_buttons' variant="outline-secondary" size="lg">Dive In Your Country</Button>
+                        <Button style={button} id='home_buttons' variant="outline-secondary" size="lg">Dive In Your Country</Button>
                     </div>
 
                     <div id='column_left'>
@@ -56,7 +69,7 @@ function Home() {
 
                     <div id='column_left'>
                         <br></br>
-                        <Button variant="outline-secondary" size="lg">Secondary</Button>
+                        <Button style={button} variant="outline-secondary" size="lg">Secondary</Button>
                     </div>
                 </Col>
                 <Col>
@@ -84,28 +97,45 @@ function Home() {
                         </Card>
                     </div>
                 </Col>
-                <Col id='column_right'>
+                <Col id='column_left'>
                     <br></br>
                     <ButtonGroup size="lg" className="mb-2">
-                        <Button variant="outline-secondary">NasDaQ <div id='value_text'>2.1% Up</div></Button>
-                        <Button variant="outline-secondary">Crude Oil <div id='value_text'>12.4$</div></Button>
-                        <Button variant="outline-secondary">Gold <div id='value_text'>20.3$</div></Button>
+                        <Button style={button} variant="outline-secondary">NasDaQ<div id='value_text'><div style={percentage_red_style}>2.1%</div></div></Button>
+                        <Button style={button} variant="outline-secondary">Tokyo<div id='value_text'><div style={percentage_green_style}>4.21%</div></div></Button>
+                        <Button style={button} variant="outline-secondary">Amsterdam <div id='value_text'><div style={percentage_red_style}>5.1%</div></div></Button>
+                    </ButtonGroup>
+                    <ButtonGroup size="lg" className="mb-2">
+                        <Button style={button} variant="outline-secondary">London<div id='value_text'><div style={percentage_green_style}>1.51%</div></div></Button>
+                        <Button style={button} variant="outline-secondary">NewYork<div id='value_text'><div style={percentage_green_style}>1.91%</div></div></Button>
+                        <Button style={button} variant="outline-secondary">HongKong <div id='value_text'><div style={percentage_red_style}>3.1%</div></div></Button>
                     </ButtonGroup>
                     <br></br>
                     <ButtonGroup size="lg" className="mb-2">
-                        <Button variant="outline-secondary">NasDaQ <div id='value_text'>2.1% Up</div></Button>
-                        <Button variant="outline-secondary">Crude Oil <div id='value_text'>12.4$</div></Button>
-                        <Button variant="outline-secondary">Gold <div id='value_text'>20.3$</div></Button>
+                        <Button style={button} variant="outline-secondary">Crude Oil <div id='value_text'><div style={percentage_green_style}>2.1%</div></div></Button>
+                        <Button style={button} variant="outline-secondary">Gold <div id='value_text'><div style={percentage_red_style}>2.1%</div></div></Button>
+                        <Button style={button} variant="outline-secondary">Silver <div id='value_text'><div style={percentage_red_style}>2.1%</div></div></Button>
                     </ButtonGroup>
 
-                    <div id='column_center'>
-                        <Button id='home_buttons' variant="outline-secondary" size="lg">Dive In Your Country</Button>
-                    </div>
+                    <Row>
+                        <Col>
+                            <div id='column_center'>
+                                <Button style={button} id='home_buttons' variant="outline-secondary" size="lg">USD</Button>
+                            </div>
+                        </Col>
+                        <Col>
+                            <div id='column_center'>
+                                <Button style={button} id='home_buttons' variant="outline-secondary" size="lg">EURO</Button>
+                            </div>
+                        </Col>
+                        <Col>
+                            <div id='column_center'>
+                                <Button style={button} id='home_buttons' variant="outline-secondary" size="lg">Pound</Button>
+                            </div>
+                        </Col>
+                    </Row>
+                    
                     <div style={button_styles} id='column_center'>
-                        <Button id='home_buttons' variant="outline-secondary" size="lg">Dive In Your Country</Button>
-                    </div>
-                    <div style={button_styles} id='column_center'>
-                        <Button id='home_buttons' variant="outline-secondary" size="lg">Dive In Your Country</Button>
+                        <Button style={button} id='home_buttons' variant="outline-success" size="lg">Other Currency Rates</Button>
                     </div>
 
                     <div id='column_left'>
@@ -114,7 +144,7 @@ function Home() {
                             <Card.Img variant="top" src="https://images.pexels.com/photos/745243/pexels-photo-745243.jpeg?auto=compress&cs=tinysrgb&w=1600" />
                             <Card.Body>
                                 <a href="/Currency">
-                                    <Card.Title>Global Poverty Rate Increased</Card.Title>
+                                    <Card.Title>China Increased GDP Growth Rate In 2022</Card.Title>
                                 </a>
                             </Card.Body>
                         </Card>
@@ -168,12 +198,12 @@ function Home() {
 
                     <div id='column_left'>
                         <br></br>
-                        <Button variant="outline-secondary" size="lg">Secondary</Button>
+                        <Button style={button} variant="outline-secondary" size="lg">Secondary</Button>
                     </div>
 
                     <div id='column_left'>
                         <br></br>
-                        <Button variant="outline-secondary" size="lg">Secondary</Button>
+                        <Button style={button} variant="outline-secondary" size="lg">Secondary</Button>
                     </div>
                 </Col>
             </Row>
