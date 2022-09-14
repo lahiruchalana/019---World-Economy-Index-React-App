8b92b7ca713d1692ee8a7c2e928c07d1166cb18c
lahiruchalana <lahiruchalanairesh@gmail.com>
2022-09-14 18:56:51 +0530

    completed footer component

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import './Footer.css'
 import * as Icon from 'react-bootstrap-icons';
+import * as LogoImage from '../Resources/WorldEconomyIndex.png'
 
 
 function Footer() {
+
 
     return(
         <div>
@@ -14,40 +16,37 @@ function Footer() {
                     <div class="col-md-3 col-sm-6">
 
                         <div class="footer-pad">
-                        <h4>Heading 1</h4>
+                            <h4>World Economy Index, Inc.</h4>
+                            <img id='logo' alt='logo' src={LogoImage.default} />                        
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+
+                        <div class="footer-pad">
+                        <h4>Deep In</h4>
                         <ul class="list-unstyled">
-                            <li><a href="/#">Payment Center</a></li>
-                            <li><a href="/#">Contact Directory</a></li>
-                            <li><a href="/#">Forms</a></li>
-                            <li><a href="/#">News and Updates</a></li>
-                            <li><a href="/#">FAQs</a></li>
+                            <li><a href="/#">Forum</a></li>
+                            <li><a href="/#">Researches</a></li>
+                            <li><a href="/#">Academy</a></li>
+                            <li><a href="/#">Investments</a></li>
+                            <li><a href="/#">Sign Up</a></li>
+                            <li><a href="/#">Login</a></li>
+                            <li><a href="/#">Privacy</a></li>
                         </ul>
                         </div>
                     </div>
                     <div class="col-md-3 col-sm-6">
 
                         <div class="footer-pad">
-                        <h4>Heading 2</h4>
+                        <h4>Latest</h4>
                         <ul class="list-unstyled">
-                            <li><a href="/#">Website Tutorial</a></li>
-                            <li><a href="/#">Accessibility</a></li>
-                            <li><a href="/#">Disclaimer</a></li>
-                            <li><a href="/#">Privacy Policy</a></li>
-                            <li><a href="/#">FAQs</a></li>
-                            <li><a href="/#">Webmaster</a></li>
-                        </ul>
-                        </div>
-                    </div>
-                    <div class="col-md-3 col-sm-6">
-
-                        <div class="footer-pad">
-                        <h4>Heading 3</h4>
-                        <ul class="list-unstyled">
-                            <li><a href="/#">Parks and Recreation</a></li>
-                            <li><a href="/#">Public Works</a></li>
-                            <li><a href="/#">Police Department</a></li>
-                            <li><a href="/#">Fire</a></li>
-                            <li><a href="/#">Mayor and City Council</a></li>
+                            <li><a href="/#">Economics</a></li>
+                            <li><a href="/#">Industry</a></li>
+                            <li><a href="/#">Market</a></li>
+                            <li><a href="/#">Politics</a></li>
+                            <li><a href="/#">Technology</a></li>
+                            <li><a href="/#">Currency</a></li>
+                            <li><a href="/#">Country</a></li>
                         </ul>
                         </div>
                     </div>
@@ -72,7 +71,7 @@ function Footer() {
                     </div>
                     <div class="row">
                         <div class="col-md-12 copy">
-                            <p class="text-center">&copy; Copyright 2018 - Company Name.  All rights reserved.</p>
+                            <p class="text-center">&copy; Copyright 2018 - World Economy Index.  All rights reserved.</p>
                         </div>
                     </div>
 
