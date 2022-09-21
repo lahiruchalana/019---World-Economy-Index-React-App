b4411d33868995a90f88a1937c8d464130c979a0
lahiruchalana <lahiruchalanairesh@gmail.com>
2022-09-21 13:29:27 +0530

    [change-url] change urls of currencyRate admin file

--- src/Views/Admin/CurrencyRates.js
+++ src/Views/Admin/CurrencyRates.js
@@ -48,10 +48,10 @@ function CurrencyRates() {
     const dateList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
     const currencyUrl = `http://localhost:8080/api/data/currency`;
-    const currencyRateGetUrl = "http://localhost:8080/api/data/currency/rate/all";
-    const currencyRatePostUrl = "http://localhost:8080/api/data/currency/rate";
-    const currencyRateUpdateUrl = "http://localhost:8080/api/data/currency/rate/update";
-    const currencyRateDeleteUrl = "http://localhost:8080/api/data/currency/rate/delete";
+    const currencyRateGetUrl = "http://localhost:8080/api/data/currencies/rates/all";
+    const currencyRatePostUrl = "http://localhost:8080/api/data/currencies/rates";
+    const currencyRateUpdateUrl = "http://localhost:8080/api/data/currencies/rates";
+    const currencyRateDeleteUrl = "http://localhost:8080/api/data/currencies/rates/delete";
 
     useEffect(() => {   // get currency data (purpose to store in drop down buttons)
         axios.get(currencyUrl).then((response) => {
@@ -240,7 +240,16 @@ function CurrencyRates() {
     }
 
     function updateData() {
-        axios.put(`${currencyRateUpdateUrl}/${updateCurrencyRateId}/${addNewCurrencyRateValue}/${addNewRecordStatus}/${addNewYear}/${addNewMonth}/${addNewDate}/${addNewCurrency.currencyId}/${addNewEqualsCurrency.currencyId}`, {})
+        axios.put(`${currencyRateUpdateUrl}/currencies/${addNewCurrency.currencyId}/currencyRates/${updateCurrencyRateId}`, {
+            currencyRateValue: addNewCurrencyRateValue,
+            recordStatus: addNewRecordStatus,
+            year: addNewYear,
+            month: addNewMonth,
+            date: addNewDate,
+            equalsCurrency: {
+                currencyId: addNewEqualsCurrency.currencyId
+            }
+        })
         .then((response) => {
             setCurrencyRateResponse(oldList => [...oldList, response.data]);
         })
